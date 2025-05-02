eedae50b1297660ac93cec6ea980c6dba3ab89a7
Alex David <alexanderadavid@users.noreply.github.com>
2025-05-02 12:03:01 -0700

    types

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,6 @@ __version__ = version("pysynthbio")
 
 # populate package namespace
 from .call_model_api import (
-    get_available_models as get_available_models,
     get_valid_modalities as get_valid_modalities,
     get_valid_query as get_valid_query,
     predict_query as predict_query,
