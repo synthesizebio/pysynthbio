33faca6ea08bc5f7e38bc4e1729c1eb4bf060164
Candace Savonen <cansav09@gmail.com>
2025-09-12 13:30:34 -0400

    Add keyring as dependency

--- src/setup.py
+++ src/setup.py
@@ -3,4 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name="pysynthbio",
     packages=find_packages(exclude=["source", "docs", "tests"]),
+    install_requires=[
+        "numpy>=2.2.5",
+        "pandas>=2.2.3",
+        "requests",
+        "keyring>=23.0.0",
+    ],
 )
